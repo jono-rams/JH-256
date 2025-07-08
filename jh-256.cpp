6718b99f797fb2a1a59e22b7b47eb0f50082f37e
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2025-07-08 08:52:04 -0400

    Helper Functions

--- jh-256.cpp
+++ jh-256.cpp
@@ -1,3 +1,5 @@
+#include <cstdint>
+
 H_INITIAL = [0x21fba37b, 0x43ab9fb6, 0x75a9f91d, 0x86305019, 0xd7cd8173, 0x07fe00ff, 0x379f513f, 0x66b651a8, 0x764ab842]
 K_CONSTANTS = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 
     0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 
@@ -5,3 +7,30 @@ K_CONSTANTS = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f
     0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 
     0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
 
+uint32_t rotr32(uint32_t value, unsigned int count) {
+    return (value >> count) | (value << (32 - count));
+}
+
+uint32_t ch(uint32_t x, uint32_t y, uint32_t z) {
+    return ((x & z) | (y & ~z));
+}
+
+uint32_t maj(uint32_t x, uint32_t y, uint32_t z) {
+    ((z & y) ^ (~x & y) | (z & x))
+}
+
+uint32_t sigma0(uint32_t x) {
+    return rotr32(x, 17) ^ rotr32(x, 23) ^ rotr32(x, 30);
+}
+
+uint32_t sigma1(uint32_t x) {
+    return rotr32(x, 3) ^ rotr32(x, 19) ^ rotr32(x, 33);
+}
+
+uint32_t sigma_ms0(uint32_t x) {
+    return rotr32(x, 11) ^ rotr32(x, 24) ^ (x >> 3);  
+}
+
+uint32_t sigma_ms1(uint32_t x) {
+    return rotr32(x, 29) ^ rotr32(x, 31) ^ (x >> 10);
+}
